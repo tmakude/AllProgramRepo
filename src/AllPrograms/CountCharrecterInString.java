d75c6f0882064887b0ac8919477f567b10c3721a
Tushar <tmakude@gmail.com>
2024-12-30 19:59:22 +0530

    chracterCount Program

--- src/AllPrograms/CountCharrecterInString.java
+++ src/AllPrograms/CountCharrecterInString.java
@@ -1,10 +1,37 @@
 package AllPrograms;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class CountCharrecterInString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String str = "geeksforgeeks";
+		countDiplicates(str);
 
+	}
+
+	private static void countDiplicates(String str) {
+		
+		String newString = "";
+		
+		Map<Character , Integer> map = new LinkedHashMap<>();
+		
+		for(char c : str.toCharArray()) {
+			
+			map.put( c , map.getOrDefault(c, 0)+1);
+		}
+		
+		for(Map.Entry<Character , Integer> entry : map.entrySet()) {
+			
+			newString += entry.getKey()+""+entry.getValue();
+		}
+		
+		System.out.println(newString);
+
+
+		
 	}
 
 }
